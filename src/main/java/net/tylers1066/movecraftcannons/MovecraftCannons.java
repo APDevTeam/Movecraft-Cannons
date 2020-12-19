609d5fcfa0f9e3a52064d8e185f7f05112f51de0
TylerS1066 <TylerS1066@users.noreply.github.com>
2020-12-19 16:23:29 -0600

    Config & internationalisation

---
+++ src/main/java/net/tylers1066/movecraftcannons/MovecraftCannons.java
@@ -0,0 +1,73 @@
+package net.tylers1066.movecraftcannons;
+
+import at.pavlov.cannons.Cannons;
+import net.countercraft.movecraft.combat.movecraftcombat.MovecraftCombat;
+import net.tylers1066.movecraftcannons.config.Config;
+import net.tylers1066.movecraftcannons.listener.ProjectileImpactListener;
+import net.tylers1066.movecraftcannons.localisation.I18nSupport;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.logging.Level;
+
+
+public final class MovecraftCannons extends JavaPlugin {
+    private static MovecraftCannons instance;
+    private static Cannons cannonsPlugin = null;
+
+    public static MovecraftCannons getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        saveDefaultConfig();
+        Config.Debug = getConfig().getBoolean("Debug", false);
+
+        // TODO other languages
+        String[] languages = {"en"};
+        for (String s : languages) {
+            if (!new File(getDataFolder()  + "/localisation/mc-cannonslang_"+ s +".properties").exists()) {
+                this.saveResource("localisation/mc-cannonslang_"+ s +".properties", false);
+            }
+        }
+        Config.Locale = getConfig().getString("Locale", "en");
+        I18nSupport.init();
+
+        Config.EnableCannonsTracking = getConfig().getBoolean("EnableCannonsTracking", true);
+
+
+        // Load cannons plugin
+        Plugin cannons = getServer().getPluginManager().getPlugin("Cannons");
+        if(!(cannons instanceof Cannons)) {
+            getLogger().log(Level.SEVERE, I18nSupport.getInternationalisedString("Cannons plugin not found"));
+        }
+        cannonsPlugin = (Cannons) cannons;
+        getLogger().info(I18nSupport.getInternationalisedString("Cannons plugin found"));
+
+
+        if(Config.EnableCannonsTracking) {
+            // Load Movecraft-Combat plugin
+            Plugin mcc = getServer().getPluginManager().getPlugin("Movecraft-Combat");
+            if (mcc instanceof MovecraftCombat) {
+                getLogger().info(I18nSupport.getInternationalisedString("Movecraft-Combat found"));
+                getServer().getPluginManager().registerEvents(new ProjectileImpactListener(), this);
+            }
+            else {
+                getLogger().info(I18nSupport.getInternationalisedString("Movecraft-Combat not found"));
+            }
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public Cannons getCannonsPlugin() {
+        return cannonsPlugin;
+    }
+}
---
+++ src/main/java/net/tylers1066/movecraftcannons/config/Config.java
@@ -0,0 +1,13 @@
+package net.tylers1066.movecraftcannons.config;
+
+
+public class Config {
+    public static boolean Debug = false;
+
+    // Localisation
+    public static String Locale = "en";
+
+    // Damage Tracking
+    public static boolean EnableCannonsTracking = false;
+}
+
