5a0bdb68932bab53c7e6ee6d6b1bb49239b761ac
TylerS1066 <TylerS1066@users.noreply.github.com>
2020-12-19 16:46:39 -0600

    Add rotation listener

---
+++ src/main/java/net/tylers1066/movecraftcannons/listener/RotationListener.java
@@ -0,0 +1,31 @@
+package net.tylers1066.movecraftcannons.listener;
+
+import at.pavlov.cannons.cannon.Cannon;
+import net.countercraft.movecraft.Rotation;
+import net.countercraft.movecraft.events.CraftRotateEvent;
+import net.tylers1066.movecraftcannons.MovecraftCannons;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.util.Vector;
+import java.util.HashSet;
+
+
+public class RotationListener implements Listener {
+    @EventHandler
+    public void rotateListener(CraftRotateEvent e) {
+        if(e.getCraft().getNotificationPlayer() == null)
+            return;
+
+        HashSet<Cannon> cannons = MovecraftCannons.getInstance().getCannons(e.getCraft().getHitBox(), e.getCraft().getW(), e.getCraft().getNotificationPlayer().getUniqueId());
+
+        Vector v = e.getOriginPoint().toBukkit(e.getCraft().getW()).toVector();
+        for(Cannon c : cannons) {
+            if(e.getRotation() == Rotation.CLOCKWISE) {
+                c.rotateRight(v);
+            }
+            else if(e.getRotation() == Rotation.ANTICLOCKWISE) {
+                c.rotateLeft(v);
+            }
+        }
+    }
+}
